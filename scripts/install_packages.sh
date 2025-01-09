cdbaaf070d9c40ca627d5d7cfb08f87fca1e3c0b
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-01-09 08:14:45 +0530

    udpate ...
     note/bash_note.txt               |  10 --
     note/docs/command.md             |  66 -----------
     note/docs/parameter_expension.md | 245 ---------------------------------------
     3 files changed, 321 deletions(-)

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
+
+set -e
+
+function cecho(){
+    local color_code=$1
+    local message=$2
+    echo -e "\033[38;5;${color_code}m${message}\033[0m"
+}
+
 function ctrl_c() {
-    echo "Exiting script..."
+    cecho 3 "Exiting script..."
     exit 1
 }
 trap ctrl_c INT
 
-major_str_packages="alacritty screenkey btop neovim eza vim fzf yazi xclip unzip wget curl gzip tar bash zsh sh firefox nvtop fastfetch glxinfo entr figlet trash-cli bat rofi vlc libreoffice-still ddgr progress polybar
+major_packages="alacritty screenkey btop neovim eza vim fzf yazi xclip unzip wget curl gzip tar bash zsh sh firefox nvtop fastfetch glxinfo entr figlet trash-cli bat rofi vlc libreoffice-still ddgr progress polybar
 nitrogen bluez bluez-utils brightnessctl feh xorg-xinput blueman xsettingsd thefuck lazygit conky ntfs-3g qt5ct lxappearance xdotool xorg-xbacklight gucharmap gimp rofi-emoji rofi-calc alsa-utils flameshot luarocks
-obs-studio xorg-xdpyinfo acpi git tmux grep ripgrep nushell jq kitty man go nodejs zoxide viu wezterm picom github-cli mesa-demos intel-media-driver nsxiv grafana bc materia-gtk-theme papirus-icon-theme python-pywal
+obs-studio xorg-xdpyinfo acpi git tmux grep ripgrep nushell jq kitty go nodejs zoxide viu wezterm picom github-cli mesa-demos intel-media-driver nsxiv grafana bc materia-gtk-theme papirus-icon-theme python-pywal
 xfce4-clipman-plugin blueberry sddm gvfs thunar-volman numactl ark reflector vkd3d man man-pages power-profiles-daemon dunst kdenlive wine man-db wireplumber tree sof-firmware winetricks udisks2 intel-ucode tldr
 cronie nmtui thunar-archive-plugin thunar-media-tags-plugin noto-fonts-emoji evtest ffmpegthumbnailer tumbler lshw php bind net-tools usbutils wireshark-qt wireshark-cli lldb cppcheck valgrind acpi_call gptfdisk
 lxsession baobab composer texlab fd mingw-w64-gcc"
 
-major_package_array=($major_str_packages)
+major_package_array=($major_packages)
 major_package_array+=("gcc clang libc++ cmake ninja libx11 libxcursor mesa-libgl fontconfig")
 major_package_array+=("python-pluggy python-pycosat python-ruamel-yaml")
 major_package_array+=("wine-gecko wine-mono lib32-glibc lib32-gcc-libs lib32-freetype lib32-libpng")
@@ -26,28 +35,31 @@ major_package_array+=("mpv xorg-xwininfo xorg-xrandr")
 major_package_array+=("zathura zathura-pdf-mupdf")
 major_package_array+=("postgresql") # remove for this system only...
 
+minimal_packages="btop neovim eza vim fzf yazi unzip wget curl gzip tat bash zsh sh fastfetch trash-cli bat ddgr brightnessctl lazygit ntfs-3g qt5ct git tmux grep ripgrep man-db zoxide mesa-demos bc tree nmtui 
+wireshark-qt wireshark-cli composer"
+
 function install_major(){
-    echo "Updating System..."
+    cecho 2 "Updating System..."
     sudo pacman -Syu
 
     for package in "${major_package_array[@]}"; do
-        echo "Installing $package..."
-        sudo pacman -S --noconfirm "$package" || { echo "Failed to install $package"; exit 1; }
+        cecho 2 "Installing $package..."
+        sudo pacman -S --noconfirm "$package" || { cecho 3 "Failed to install $package"; exit 1; }
     done
 
     read -p "Install Yay (Y|n)? " install_yay
     if [ -z $install_yay ] || [ "${install_yay,,}" == "y"]; then
-        echo "Install Yay..."
+        cecho 2 "Install Yay..."
         sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
         cd ~
     fi
 
-    echo "Install Package from Yay...(Will have to do it manually.)"
+    cecho 2 "Install Package from Yay...(Will have to do it manually.)"
     yay -S --noconfirm iriunwebcam-bin brave whatsie-git signal-desktop ascii-image-converter ani-cli getnf rxvt-unicode also-utils pywal spotify-adblock-git unicode autotiling-rs-git ascii-draw \
     aseprite kotatsu-dl-git rofi-greenclip tokyonight-gtk-theme-git qt5-graphicaleffects geogebra-6-electron tensorrt dxvk-bin dxvk terraform  \
     aws-cli-v2-bin xwinwrap-git gowall yt-dlp ttf-ms-win11-auto
 
-    echo "Install Anaconda..."
+    cecho 2 "Install Anaconda..."
     wget -P ~/Downloads/ https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
     chmod +x ~/Downloads/Anaconda3-2024.06-1-Linux-x86_64.sh
     bash ~/Downloads/Anaconda3-2024.06-1-Linux-x86_64.sh
@@ -59,5 +71,7 @@ function install_major(){
 }
 
 function install_minimal(){
-
+    cecho 2 "Updating System..."
+    sudo pacman -Syu
 }
+
